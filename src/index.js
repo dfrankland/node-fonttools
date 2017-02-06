022cee4b8db546ec41e6226f2117461d6bc291cc
Dylan Frankland <dylan@frankland.io>
2017-02-05 20:08:15 -0800

    Adjust font tools path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { resolve as resolvePath } from 'path';
 import fonttools from './Release/fonttools.node';
 
-export const DEFAULT_FONTTOOLS = resolvePath(__dirname, './fonttools/Lib');
+export const DEFAULT_FONTTOOLS = resolvePath(__dirname, '../fonttools/Lib');
 
 export default (fonttoolsPath = DEFAULT_FONTTOOLS) => (
   fonttools(fonttoolsPath)
