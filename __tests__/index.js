0586e6064cb88069fd6a9983660f535e56b497ab
Dylan Frankland <dylan@frankland.io>
2018-07-03 09:08:29 -0700

    Improve tests by adding XML validation

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,12 +1,15 @@
-const { readFileSync } = require('fs');
-const { resolve: resolvePath } = require('path');
-const { decompile } = require('..');
+import { readFileSync } from 'fs';
+import { resolve as resolvePath } from 'path';
+import fastXmlParser from 'fast-xml-parser';
+import { decompile } from '..';
 
-const ttf = readFileSync(resolvePath(__dirname, './PlayfairDisplay-Regular.ttf'));
+const ttf = readFileSync((
+  resolvePath(__dirname, './PlayfairDisplay-Regular.ttf')
+));
 
 describe('fonttools', () => {
   it('can decompile TTF fonts to XML', () => {
     const ttx = decompile(ttf).toString('utf8');
-    expect(typeof ttx).toBe('string');
+    expect(fastXmlParser.validate(ttx)).toBe(true);
   });
 });
